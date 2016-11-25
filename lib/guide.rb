60bb23de9aa9a8398f7373367e5a520073451002
Sam Lin <greenlin111@gmail.com>
2016-11-25 17:59:49 +0800

    create support dir and create number_helper and string_extend

--- lib/guide.rb
+++ lib/guide.rb
@@ -1,4 +1,5 @@
 require 'restaurant'
+require 'support/string_extend'
 
 class Guide 
 
@@ -63,16 +64,14 @@ class Guide
   end
 
   def list
-    puts "\nListing restaurants".upcase
+    output_action_header("Listing restaurants")
     restaurants = Restaurant.saved_restaurants
-    restaurants.each do |restaurant|
-      puts restaurant.name + " | " + restaurant.cuisine + " | " + restaurant.price 
-    end
+    output_restaurant_table(restaurants)
 
   end
 
   def add
-    puts "\n\n Add a restaurant".upcase
+    output_action_header("Add a restaurant")
     
     restaurant = Restaurant.build_using_questions
 
@@ -91,6 +90,27 @@ class Guide
   def conclusion
     puts "\n <<< Goodbye and Bon Appetit! >>> \n\n\n"
 
+  end
+
+  private
+
+  def output_action_header(text)
+    puts "\n#{text.upcase.center(60)}\n\n"
+  end
+
+  def output_restaurant_table(restaurants=[])
+    print " " + "Name".ljust(30)
+    print " " + "Cuisine".ljust(20)
+    print " " + "Price".rjust(6) + "\n"
+    puts "-" * 60
+    restaurants.each do |rest|
+      line =  " " << rest.name.titleize.ljust(30)
+      line << " " + rest.cuisine.titleize.ljust(20)
+      line << " " + rest.formatted_price.rjust(6)
+      puts line
+    end
+    puts "No listings found" if restaurants.empty?
+    puts "-" * 60
   end
 
 end
